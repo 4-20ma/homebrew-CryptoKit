212d316ce066c6e1dbd80c54654901c6b7d23a41
Doc Walker <4-20ma@wvfans.net>
2026-05-02 20:58:32 -0500

    Update URL for CryptoKitCLI repository

--- Formula/crypto_kitcli.rb
+++ Formula/crypto_kitcli.rb
@@ -1,7 +1,7 @@
 class CryptoKitcli < Formula
   desc "CryptoKitCLI"
   homepage "https://github.com/4-20ma/CryptoKitCLI"
-  url "https://github.com/4-20ma/CryptoKitCLI/archive/0.1.0.tar.gz"
+  url "https://github.com/alexruperez/CryptoKitCLI/archive/0.1.0.tar.gz"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   head "https://github.com/4-20ma/CryptoKitCLI.git"
 
