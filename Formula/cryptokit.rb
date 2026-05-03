f4232e6797e24b824a518522b9d0239dcca33303
Doc Walker <4-20ma@wvfans.net>
2026-05-02 21:34:30 -0500

    Update homepage and repository URLs for Cryptokit

--- Formula/cryptokit.rb
+++ Formula/cryptokit.rb
@@ -1,9 +1,9 @@
 class Cryptokit < Formula
   desc "CryptoKit"
-  homepage "https://github.com/4-20ma/CryptoKitCLI"
-  url "https://github.com/4-20ma/homebrew-CryptoKitCLI/archive/refs/tags/0.1.0.tar.gz"
+  homepage "https://github.com/4-20ma/CryptoKit"
+  url "https://github.com/4-20ma/homebrew-CryptoKit/archive/refs/tags/0.1.0.tar.gz"
   sha256 "ff53e2d38338ff9af28ab3c242eece05d2ffb22240fa57b16ca6d9da307d20f0"
-  head "https://github.com/4-20ma/CryptoKitCLI.git"
+  head "https://github.com/4-20ma/CryptoKit.git"
 
   depends_on xcode: ["11.4", :build]
 
