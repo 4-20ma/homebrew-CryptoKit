b0f6f501405ada0a08459f90f4fae4d303469a05
Doc Walker <4-20ma@wvfans.net>
2026-05-02 20:52:48 -0500

    Add CryptoKitCLI formula for Homebrew

--- Formula/crypto_kitcli.rb
+++ Formula/crypto_kitcli.rb
@@ -1,1 +1,13 @@
+class CryptoKitcli < Formula
+  desc "CryptoKitCLI"
+  homepage "https://github.com/20ma/CryptoKitCLI"
+  url "https://github.com/20ma/CryptoKitCLI/archive/0.1.0.tar.gz"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  head "https://github.com/4-20ma/CryptoKitCLI.git"
 
+  depends_on xcode: ["11.4", :build]
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+end
