28f6f3db22c0da1ba86c4c2c5e72af724b4f90d8
Doc Walker <4-20ma@wvfans.net>
2026-05-02 21:05:37 -0500

    Update sha256 checksum for CryptoKitCLI formula

--- Formula/crypto_kitcli.rb
+++ Formula/crypto_kitcli.rb
@@ -2,7 +2,7 @@ class CryptoKitcli < Formula
   desc "CryptoKitCLI"
   homepage "https://github.com/4-20ma/CryptoKitCLI"
   url "https://github.com/4-20ma/homebrew-CryptoKitCLI/archive/refs/tags/0.1.0.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "ff53e2d38338ff9af28ab3c242eece05d2ffb22240fa57b16ca6d9da307d20f0"
   head "https://github.com/4-20ma/CryptoKitCLI.git"
 
   depends_on xcode: ["11.4", :build]
