7c8c13f1b937f51913da4acd0d9bd93f94f622d5
Doc Walker <4-20ma@wvfans.net>
2026-05-02 21:14:33 -0500

    Fix description for CryptoKit formula

--- Formula/cryptokit.rb
+++ Formula/cryptokit.rb
@@ -1,5 +1,5 @@
 class Cryptokit < Formula
-  desc "CryptoKitCLI"
+  desc "CryptoKit"
   homepage "https://github.com/4-20ma/CryptoKitCLI"
   url "https://github.com/4-20ma/homebrew-CryptoKitCLI/archive/refs/tags/0.1.0.tar.gz"
   sha256 "ff53e2d38338ff9af28ab3c242eece05d2ffb22240fa57b16ca6d9da307d20f0"
