15d8ad1003b73202eded99d9837e12421eef5a6c
Doc Walker <4-20ma@wvfans.net>
2026-05-02 21:35:06 -0500

    Update sha256 checksum for CryptoKit formula

--- Formula/cryptokit.rb
+++ Formula/cryptokit.rb
@@ -2,7 +2,7 @@ class Cryptokit < Formula
   desc "CryptoKit"
   homepage "https://github.com/4-20ma/CryptoKit"
   url "https://github.com/4-20ma/homebrew-CryptoKit/archive/refs/tags/0.1.0.tar.gz"
-  sha256 "ff53e2d38338ff9af28ab3c242eece05d2ffb22240fa57b16ca6d9da307d20f0"
+  sha256 "0db610038f871556f4e60f92a85bb671d4cc7ed4f6eee94680cfa14943194258"
   head "https://github.com/4-20ma/CryptoKit.git"
 
   depends_on xcode: ["11.4", :build]
